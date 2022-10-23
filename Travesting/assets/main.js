f71c22c42a14f7fa72a64710b66d2cb4ec2911ac
Sabrinashh <110469877+Sabrinashh@users.noreply.github.com>
2022-10-23 12:22:41 +0400

    file

--- Travesting/assets/main.js
+++ Travesting/assets/main.js
@@ -39,13 +39,15 @@ function currentSlide(n) {
 }
 
 function showSlides(n) {
-    let i;
     let slides = document.getElementsByClassName("mb-3");
     if (n > slides.length) {slideIndex = 1}
     if (n < 1) {slideIndex = slides.length}
     for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
     }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
     slides[slideIndex-1].style.display = "block";
+    setTimeout(showSlides, 3000); 
   }
 
